75bb5ab8eb6f13427a2ea6d5123e414c46eef89b
aupous <tuan.phamvan.m@gmail.com>
2021-02-03 09:42:14 +0700

    Add project files.

---
+++ ConsoleAppWithForm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleAppWithForm
+{
+    class Program
+    {
+        static System.Timers.Timer timer = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter key Q to exit!");
+            runForever();
+            Application.Run(new WindowsFormsApp.Form1());
+            
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'Q' && timer != null)
+            {
+                timer.Stop();
+            }
+        }
+
+        public static void runForever()
+        {
+            timer = new System.Timers.Timer(2000);
+            timer.Elapsed += Timer_ElapsedAsync;
+            timer.Start();
+        }
+
+        public static void Timer_ElapsedAsync(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
